bff37d394442863afda73cd6df1d9c7116f6ed32
𝗥𝗶𝘀𝗵𝗶 𝗥𝗼𝘆 <rishijsr2006@gmail.com>
2026-06-27 10:37:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Operators(Relational) | Set - 2/java-operators-relational-set-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    static void relationalOperators(int A, int B) {
+        // code here
+        if(A>B){
+            System.out.print(A+" is greater than "+B);
+            System.out.println();
+        }
+        else if(A<B){
+            System.out.print(A+" is less than "+B);
+            System.out.println();
+        }
+        else{
+         System.out.print(A+" is equal to "+B);   
+         System.out.println();
+        }
+    }
+}
